732fae0cd3d9e2346b9516b70e68142f94e0010f
zhanglibo <zhanglibo@126.com>
2018-12-28 23:04:10 +0800

    登录验证

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,19 @@
 import axios from 'axios'
 // 设置请求的基准路径
 axios.defaults.baseURL = 'http://47.96.21.88:8888/api/private/v1/'
+// 登录拦截器
+// // 拦截器处理token
+// axios.interceptors.request.use(function (config) {
+//   // 获取当前是否存在token
+//   let token = localStorage.getItem('mytoken')
+//   if (token) {
+//     // Authorization 请求头的键是前后台约定的
+//     config.headers['Authorization'] = token
+//   }
+//   return config
+// }, function (error) {
+//   return Promise.reject(error)
+// })
 // 实现登录功能
 export const login = (params) => {
   return axios.post('login', params).then(
